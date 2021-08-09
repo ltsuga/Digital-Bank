ce50ce82d97a95410cfbcba9fe90296400f6ca0d
ltsuga <85001056+ltsuga@users.noreply.github.com>
2021-08-08 23:19:12 -0300

    create person class

---
+++ src/main/kotlin/com/digitalbank/class/Pessoa.kt
@@ -0,0 +1,14 @@
+package com.digitalbank.`class`
+
+class Pessoa {
+    var nome: String = "Leo"
+    var cpf: String = "123.234.568-11"
+
+}
+
+fun main(){
+    val leo = Pessoa()
+
+    println(leo.nome)
+    println(leo.cpf)
+}
